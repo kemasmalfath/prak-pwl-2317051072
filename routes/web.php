9fbf227fdd0c56d2c42ff442f0ad748bbfa9c5e0
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2025-12-04 12:37:33 +0700

    Selesai Langkah Praktikum Modul 4

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\ProfileController;
 
+use App\Http\Controllers\UserController;
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
 Route::get('/profile', [ProfileController::class, 'profile']);
 
 Route::get('/', function () {
