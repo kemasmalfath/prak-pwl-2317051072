fafd5e2dde8f6d1a5f72028ac539113328fae059
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2025-12-04 12:38:53 +0700

    Tugas Pertemuan 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    @yield('content')
+    @include('components.navbar')
+    
+    <main>
+        @yield('content')
+    </main>
+    
+    @include('components.footer')
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
